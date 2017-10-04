a5d863159489d89fc150e989a952855c07207af6
Thomas Duboucher <thomas@duboucher.eu>
2017-10-04 15:02:11 +0200

    cleanup regex

--- lib/parser.ts
+++ lib/parser.ts
@@ -12,22 +12,21 @@ class Option {
     constructor(public name: string, public value: string) {}
 }
 
-const name = p.regexp(/[a-z]+/).map(_ => _[0]);
-const rest = p.regexp(/[^\r\n]*/).map(_ => _[0]);
+const name = p.regexp(/[a-zA-Z][a-zA-Z0-9]*([-_.][a-zA-Z0-9]+)*/).map(_ => _[0]);
+const rest = p.regexp(/[^\r\n\u0085\u2028\u2029]*/).map(_ => _[0]);
 const colon = p.string(":").q;
 const semicolon = p.string(";").q;
 const equal = p.string("=").q;
-const indent = p.regexp(/(    |\t)/).q;
-const whitespace = p.regexp(/[ \t]*/).q;
-const whitespace1 = p.regexp(/[ \t]/).q;
-const newline = p.regexp(/(\r\n|\r|\n)/).q;
-const newlinen = p.regexp(/(\r\n|\r|\n)*/).q;
+const indent = p.regexp(/[ \f\t\v\u00A0]+/).q;
+const whitespace = p.regexp(/[ \f\t\v\u00A0]*/).q;
+const whitespace1 = p.regexp(/[ \f\t\v\u00A0]/).q;
+const newline = p.regexp(/[\n\u0085\u2028\u2029]|\r\n?/).q;
+const newlinen = p.regexp(/([\n\u0085\u2028\u2029]|\r\n?)*/).q;
 
 const comment = semicolon.then(rest).map(_ => new Comment(_.trim()));
 
 const option = indent.then(name).then(whitespace).then(equal).then(rest).map(_ => new Option(_[0], _[1].trim()));
-const options = option.manySepBy(newline);
-const declaration = name.then(whitespace).then(colon).then(name.manySepBy(whitespace));
+const declaration = name.then(whitespace).then(colon).then(name.manySepBy(whitespace)).map(_ => new Declaration(_[0], _[1]));
 
 const configuration = comment.or(declaration).or(option).manySepBy(newlinen).between(newlinen).then(p.eof);
 
