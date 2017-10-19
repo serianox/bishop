30d1ac74b0bdb939d2b7fe8113a3fcd2f16317fb
Thomas Duboucher <thomas@duboucher.eu>
2017-10-19 23:02:23 +0200

    adding argument parsing

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,1 +1,7 @@
-import * as commander from "commander";
+import * as program from "commander";
+
+program
+    .version("0.1.0")
+    .option("-f, --file <file>", "bishop file");
+
+program.parse(process.argv);
