d2126aeceb65968f7be868f837d41d6a790ea54a
Thomas Duboucher <thomas@duboucher.eu>
2017-10-19 23:10:00 +0200

    enhance options parsing

--- lib/cli.ts
+++ lib/cli.ts
@@ -1,7 +1,13 @@
 import * as program from "commander";
 
+interface Options {
+    file?: string;
+}
+
 program
     .version("0.1.0")
     .option("-f, --file <file>", "bishop file");
 
 program.parse(process.argv);
+
+const opts = program.opts as Options;
