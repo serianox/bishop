88f087ec829ca71aa772cd5a5fa1a1f623f203d3
Thomas Duboucher <thomas@duboucher.eu>
2017-10-23 23:18:59 +0200

    cleanup options

--- lib/cli.ts
+++ lib/cli.ts
@@ -2,12 +2,14 @@ import * as program from "commander";
 
 interface Options {
     file?: string;
+    args: string[];
 }
 
 program
     .version("0.1.0")
+    .usage("options] <task ...>")
     .option("-f, --file <file>", "bishop file");
 
 program.parse(process.argv);
 
-const opts = program.opts as Options;
+const options = program as Options;
