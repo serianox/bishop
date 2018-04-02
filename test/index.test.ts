a9914fd6fcaf389b56f88d6cf1e719254145d0e6
Thomas Duboucher <thomas@duboucher.eu>
2018-04-02 18:29:16 +0200

    add test for unresolved goal

--- test/index.test.ts
+++ test/index.test.ts
@@ -27,6 +27,14 @@ task3:
             const tasks = Run.getInstance(data, ["task1"]);
             assert.instanceOf(tasks, BSError);
         });
+        test("unresolved goal", () => {
+            const data = `
+task1: task2 task3
+task3:
+`;
+            const tasks = Run.getInstance(data, ["task2"]);
+            assert.instanceOf(tasks, BSError);
+        });
         test(".bishop file", () => {
             const tasks = Run.getInstance(path.parse(".bishop"), ["ci"]);
             assert.instanceOf(tasks, Run);
