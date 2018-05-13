3b8707d33f87a917b946dd5a5e13ccf21fa80714
Thomas Duboucher <thomas@duboucher.eu>
2018-05-13 18:17:18 +0200

    update documentation

--- lib/error.ts
+++ lib/error.ts
@@ -1,4 +1,18 @@
+/**
+ * The class used for Bishop error management. It can compose from an existing `Error` instance and update the
+ * stacktrace accordingly.
+ */
 export class BSError extends Error {
+    /**
+     * Build an error from a message, or compose from an existing error and optionaly add a message.
+     * If `message` is an instance of `string`, build an instance of `BSError` and use the message as the error
+     * message. If `message` is an instance of `Error`, build an instance of `BSError` and keep the information
+     * form the original error. If `message` is an instance of `string` and `cause` is specified, build an instance of
+     * `BSError`, keep the stacktrace information of the original cause and update the message.
+     *
+     * @param message the message of the error
+     * @param cause the original `Error` causing this error
+     */
     constructor(message: string | Error, cause?: Error) {
         if (message instanceof Error) {
             cause = message;
@@ -10,6 +24,17 @@ export class BSError extends Error {
         Object.setPrototypeOf(this, BSError.prototype);
 
         if (cause !== undefined) {
+            // The idea is to compose a new stacktrace to both keep the location of the cause and still have the
+            // location of the error we are creating. We compare both traces line per line from the bottom. Once we
+            // reach a difference, we put on top the remainder of the one of the new error on top, then the one of the
+            // cause.
+            //
+            // Cause         Error         BSError
+            //   at file:10    at file:20    at file:10
+            //   at file:30    at file:21    at file:20
+            //   at file:31    at file:30    at file:21
+            //                 at file:31    at file:30
+            //                               at file:31
             const thisstack = this.stack!.split("\n");
             const causestack = cause.stack!.split("\n");
 
--- lib/logging.ts
+++ lib/logging.ts
@@ -1,3 +1,6 @@
+/**
+ * Logging levels.
+ */
 export enum Level {
     DEBUG,
     INFO,
@@ -5,22 +8,38 @@ export enum Level {
     ERROR,
 }
 
+/** The current global log level. */
 let loglevel: Level = Level.INFO;
 
+/**
+ * Set current logging level. All messages strictly below logging level are discarded.
+ *
+ * @param level the new logging level
+ */
 export const setLevel = (level: Level) => {
     loglevel = level;
 };
 
+/**
+ * Create a logging function from a log level.
+ *
+ * @param level the log level for the created function
+ * @returns a logging level for the level given in paramter
+ */
 const log = (level: Level) => (message: string, ...parameters: any[]): void => {
     if (loglevel <= level) {
         console.log(message, ...parameters);
     }
 };
 
+/** Log a debug message. */
 export const debug = log(Level.DEBUG);
 
+/** Log an informative message. */
 export const info = log(Level.INFO);
 
+/** Log a warning. Execution should resume when an error is logged. */
 export const warn = log(Level.WARNING);
 
+/** Log an error. Execution should not resume when an error is logged. */
 export const err = log(Level.ERROR);
