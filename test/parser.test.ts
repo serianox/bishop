efcb37965ed283283f5b1a10484a99c507bdd441
Thomas Duboucher <thomas@duboucher.eu>
2018-05-19 10:58:12 +0200

    cleanup tests

--- test/parser.test.ts
+++ test/parser.test.ts
@@ -1,47 +1,48 @@
 import { assert } from "chai";
 // import { dedent } from "dedent-js";
+import { BSError } from "../lib/error";
 import { parseConfiguration } from "../lib/parser";
 
 suite("Functional", () => {
     suite("#parseConfiguration()", () => {
         test("invalid configuration return string", () => {
-            assert.isTrue(parseConfiguration(`&`) instanceof Error);
+            assert.isTrue(parseConfiguration(`&`) instanceof BSError);
         });
         test("should parse empty string", () => {
-            assert.isFalse(parseConfiguration(``) instanceof Error);
+            assert.isFalse(parseConfiguration(``) instanceof BSError);
         });
         test("should parse newline", () => {
             assert.isFalse(parseConfiguration(`
-`) instanceof Error);
+`) instanceof BSError);
         });
         test("should parse single comment", () => {
-            assert.isFalse(parseConfiguration(`; foo`) instanceof Error);
+            assert.isFalse(parseConfiguration(`; foo`) instanceof BSError);
         });
         test("should parse multiple comments", () => {
             assert.isFalse(parseConfiguration(`; foo
-; bar`) instanceof Error);
+; bar`) instanceof BSError);
         });
         test("should parse single task", () => {
-            assert.isFalse(parseConfiguration(`task :`) instanceof Error);
+            assert.isFalse(parseConfiguration(`task :`) instanceof BSError);
         });
         test("should parse single task with one dependency", () => {
-            assert.isFalse(parseConfiguration(`task : depa`) instanceof Error);
+            assert.isFalse(parseConfiguration(`task : depa`) instanceof BSError);
         });
         test("should parse single task with many dependencies", () => {
-            assert.isFalse(parseConfiguration(`task : depa depb depn`) instanceof Error);
+            assert.isFalse(parseConfiguration(`task : depa depb depn`) instanceof BSError);
         });
         test("should parse single task with one option", () => {
             assert.isFalse(parseConfiguration(`task :
-    opta = vala`) instanceof Error);
+    opta = vala`) instanceof BSError);
         });
         test("should parse single task with two options", () => {
             assert.isFalse(parseConfiguration(`task :
     opta = vala
-    optb = valb`) instanceof Error);
+    optb = valb`) instanceof BSError);
         });
         test("should parse an option with no value", () => {
             assert.isFalse(parseConfiguration(`task :
-    opta `) instanceof Error);
+    opta `) instanceof BSError);
         });
         test("should parse complete example", () => {
             assert.isFalse(parseConfiguration(`
@@ -58,7 +59,7 @@ task2:
     optb=valb ;
 
 task3 :
-`) instanceof Error);
+`) instanceof BSError);
         });
     });
 });
